8ad75ab1bc09f38cd73ec4237c07724308430315
OM <ole@falanx.de>
2022-12-09 16:18:08 +0100

    work in progress
    the main file needs to loop through iplist to create clients. 
    Still have to check the ports of all of this, so far completely untested, but backbone is there
    i tried to follow the tutorial on services on the RPyC website

--- RPyC_Main.py
+++ RPyC_Main.py
@@ -1,3 +1,10 @@
-import rpyc
+import RPyC_client as RC
+import RPyC_service as RS
+
+
+
+
+IPlist = RC.importIP(ip.txt)
+myServer = RS.StartServer(RS.SearchService)
 
 
--- RPyC_client.py
+++ RPyC_client.py
@@ -1,3 +1,24 @@
-import RPyC
+import rpyc
+
+
+def importIP(inputfile) #reads file and returns all ip from list
+    # opening the file in read mode
+    my_file = open(inputfile "r")
+  
+    # reading the file
+    data = my_file.read()
+      
+    # replacing end of line('/n') with ' ' and
+    # splitting the text it further when '.' is seen.
+    data_into_list = data.replace('\n', ' ').split(".")
+      
+    # returning the data
+        return(data_into_list)
+
+
+class nodeClient:
+    def __init__(address, port=18861):
+        self.c = rpyc.connect("localhost", 18861)
+        self.cmd = c.root
 
 
